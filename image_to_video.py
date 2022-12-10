2ac6aa5f2b300c51d0bd652ae8bac60312fc0f9c
suyogster <suyogshrestha.tech@gmail.com>
2022-12-10 22:02:54 +0545

    text to speech updated

--- image_to_video.py
+++ image_to_video.py
@@ -2,18 +2,18 @@ from moviepy.editor import *
 from pathlib import Path
 
 img_clips = []
-path_list=[]
+path_list = []
 lock = 0
-#accessing path of each image
-img_path = '/home/spygaurad/Documents/images_delete/'
+# accessing path of each image
+img_path = './images/'
 for image in os.listdir(img_path):
     if image.endswith(".jpg"):
         path_list.append(os.path.join(img_path, image))
-#creating slide for each image
+# creating slide for each image
 # print(path_list)
 path_list.sort()
 for img_path in path_list:
-    slide = ImageClip(img_path,duration=5)
+    slide = ImageClip(img_path, duration=5)
     img_clips.append(slide)
 
 video = concatenate_videoclips(img_clips, method='compose')
@@ -24,13 +24,21 @@ subclips = []
 no_of_clips = 7
 x = 0
 for i in range(no_of_clips):
-    clip = clip1.subclip(x,x+5)
+    clip = clip1.subclip(x, x+5)
     clip = clip.crossfadeout(1.0)
     clip = clip.crossfadein(2.0)
     subclips.append(clip)
-    x +=5
-  
+    x += 5
+
 # showing final clip
-final = concatenate_videoclips(subclips, method='compose')
+audio0 = AudioFileClip('./audio/audio0.mp3')
+audio1 = AudioFileClip('./audio/audio1.mp3')
+
+final_audio_clip = []
+final_audio_clip.append(audio0)
+final_audio_clip.append(audio1)
+final_audio = concatenate_audioclips(final_audio_clip, method='compose')
+final = concatenate_videoclips(
+    subclips, method='compose').set_audio(final_audio)
 final.write_videofile("Generated_story.mp4", fps=24)
-os.remove("output.mp4")
+os.remove("./videos/output.mp4")
--- text_to_speech_converter.py
+++ text_to_speech_converter.py
@@ -1,8 +1,9 @@
 import requests
 from api_key import UBER_DUCK_API_SECRET, UBER_DUCK_KEY
 
-payload = {"speech": "Once upon a time there was a young child who lived in a small village near the woods. The child was a curious one and loved to explore the woods, but one day the child lost their way. The sun was setting and the little one was scared and alone, not knowing what to do.The child stumbled through the trees, but it was getting dark and they could no longer see the way. Suddenly, through the fog, the child heard a faint sound of music. Following the sound, the child eventually stumbled upon a small clearing in the woods. There were two people playing music together, an old man with a guitar and a young woman with a fiddle.The old man noticed the child and smiled warmly. He said, “It looks like you’ve lost your way. Don’t worry, I will take you home.”The old man and the young woman walked the child back to their village. As they walked, the old man explained to the child that through life, there will always be moments of fear and uncertainty, but if we remain hopeful and have faith, then we will eventually find our way back home.",
-           "voice": "zwf"}
+speeches = ['Once upon a time there was a young child who lived in a small village near the woods. The child was a curious one and loved to explore the woods, but one day the child lost their way. The sun was setting and the little one was scared and alone, not knowing what to do.The child stumbled through the trees, but it was getting dark and they could no longer see the way. Suddenly, through the fog, the child heard a faint sound of music. Following the sound, the child eventually stumbled upon a small clearing in the woods. There were two people playing music together, an old man with a guitar and a young woman with a fiddle.',
+            'The old man noticed the child and smiled warmly. He said, “It looks like you’ve lost your way. Don’t worry, I will take you home.”The old man and the young woman walked the child back to their village. As they walked, the old man explained to the child that through life, there will always be moments of fear and uncertainty, but if we remain hopeful and have faith, then we will eventually find our way back home.']
+
 url = "https://api.uberduck.ai/speak"
 
 headers = {
@@ -11,20 +12,23 @@ headers = {
     "content-type": "application/json"
 }
 
-speak_response = requests.post(url, json=payload, headers=headers, auth=(
-    UBER_DUCK_KEY, UBER_DUCK_API_SECRET))
+for i in range(len(speeches)):
+    payload = {"speech": speeches[i], "voices": "zwf"}
+    speak_response = requests.post(url, json=payload, headers=headers, auth=(
+        UBER_DUCK_KEY, UBER_DUCK_API_SECRET))
+    print(f'i: {i}', speak_response.json()["uuid"])
 
-print(speak_response.json()["uuid"])
-
-status_payload = {"uuid": speak_response.json()["uuid"]}
-
-headers1 = {"accept": "application/json"}
-received = False
-while received is False:
-    print("Fetching data.....")
-    status_response = requests.get(
-        f'https://api.uberduck.ai/speak-status?uuid={speak_response.json()["uuid"]}', headers=headers1, auth=(
-            UBER_DUCK_KEY, UBER_DUCK_API_SECRET))
-    print(status_response.text)
-    if status_response.json()["path"] is not None:
-        received = True
+    headers1 = {"accept": "application/json"}
+    received = False
+    while received is False:
+        print("Fetching data.....")
+        status_response = requests.get(
+            f'https://api.uberduck.ai/speak-status?uuid={speak_response.json()["uuid"]}', headers=headers1, auth=(
+                UBER_DUCK_KEY, UBER_DUCK_API_SECRET))
+        if status_response.json()["path"] is not None:
+            received = True
+            path = status_response.json()["path"]
+            print(path)
+            path_response = requests.get(path)
+            open(f'./audio/audio{i}.mp3', 'wb').write(path_response.content)
+            print(f'audio{i} downloaded')
