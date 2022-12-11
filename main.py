af4ff566a4fc51623083ae92d342cd3bc5132789
spygaurad <suraj.prasai97@gmail.com>
2022-12-11 12:01:35 +0545

    Main method added

---
+++ main.py
@@ -0,0 +1,9 @@
+#
+from story_generator import gradio_input_to_story
+# from 
+
+if __name__ == '__main__':
+    # get gradio text
+    gradio_input = ''
+    story = gradio_input_to_story(gradio_input)
+    story_to_uberduck_voice = ''
--- story_generator.py
+++ story_generator.py
@@ -1,22 +1,32 @@
 #
 import os
 import openai
-from api_key import KEY
+from api_keys import OPEN_AI_API_KEY
 
-key = KEY
+
 # openai.api_key = os.getenv(key)
-openai.api_key = key
+# openai.api_key = key
 
-prompt = "Write me a short story with an ending about a child lost in the woods but there is hope."
+# prompt = "Write me a short story with an ending about a child lost in the woods but there is hope."
 
-response = openai.Completion.create(
-  model="text-davinci-003",
-  prompt=prompt,
-  temperature=0.7,
-  max_tokens=256,
-  top_p=1,
-  frequency_penalty=0,
-  presence_penalty=0
-)
 
-print(response)
+
+def get_story(prompt):
+  openai.api_key = OPEN_AI_API_KEY
+  response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=prompt,
+    temperature=0.7,
+    max_tokens=256,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+    )
+  
+  return response
+
+def gradio_input_to_story(gradio_input):
+  # prompt = ''
+  formatted_prompt = "Write me a short story with an ending about " + gradio_input
+  story = get_story(formatted_prompt)
+  return story
